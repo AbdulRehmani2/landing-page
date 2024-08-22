bdb98f4f66bc2687089f6c2a7e865662cb98691f
AbdulRehmani2 <abdulrehmani2238@gmail.com>
2024-08-22 09:58:54 +0500

    Added metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,11 +3,23 @@ const poppins = Poppins({ weight: "500", subsets: ["latin"] });
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import Head from "next/head";
 
 import { Poppins } from "next/font/google";
 
 export const metadata: Metadata = {
-  title: "Ecotech Solutions",
+  metadataBase: new URL("https://landing-page-taupe-five-79.vercel.app/"),
+  title: "Ecotech Solutions | Save Enviornment",
+  keywords: ["Ecotech", "Technology", "Ecofriendly", "Software"],
+  openGraph: {
+    title: "Ecotech Solutions",
+    description:
+      "EcoTech Solutions is a leading provider of sustainable technology solutions.",
+    type: "website",
+    locale: "en-us",
+    url: "https://landing-page-taupe-five-79.vercel.app/",
+    siteName: "Ecotech",
+  },
   description:
     "EcoTech Solutions is a leading provider of sustainable technology solutions. We are committed to helping businesses around the world achieve their environmental goals by offering innovative, eco-friendly products and services. Our mission is to make sustainability accessible, affordable, and efficient for everyone",
 };
@@ -19,6 +31,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="shorcut icon" href="/favicon.png"></link>
+      </Head>
       <body className={poppins.className}>
         <Navbar></Navbar>
         {children}
--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,12 @@ import Seperator from "@/components/Seperator";
 import Services from "@/components/Services";
 import Testimonials from "@/components/Testimonials";
 import Contact from "@/components/Contact";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Home Page | Ecotech Solutions",
+  description: "Ecotech website's main page.",
+};
 
 function Home() {
   return (
